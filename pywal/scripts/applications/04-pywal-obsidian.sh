88129521a5a2b99e3af6e463ed51002cfa5294c1
jliima <liimatainen.jere@gmail.com>
2025-06-11 12:25:00 +0300

    Added work profile support for pywal obsidian.

--- pywal/scripts/applications/04-pywal-obsidian.sh
+++ pywal/scripts/applications/04-pywal-obsidian.sh
@@ -6,9 +6,13 @@ CACHE_DIR="$HOME/.cache/wal"
 
 
 SOURCE_FILE="$CACHE_DIR/colors-obsidian.css"
-TARGET_DIR="$HOME/Obsidian/default_vault/.obsidian/themes/PywalColors"
-TARGET_FILE="$TARGET_DIR/theme.css"
+TARGET_DIR_1="$HOME/Obsidian/default_vault/.obsidian/themes/PywalColors"
+TARGET_DIR_2="$HOME/Obsidian/work_vault/.obsidian/themes/PywalColors"
+TARGET_FILE_1="$TARGET_DIR_1/theme.css"
+TARGET_FILE_2="$TARGET_DIR_2/theme.css"
 
-# Create target directory if it doesn't exist
-mkdir -p "$TARGET_DIR"
-cp "$SOURCE_FILE" "$TARGET_FILE" && echo "Copied $SOURCE_FILE to $TARGET_FILE"
+# Create target directories if it doesn't exist
+mkdir -p "$TARGET_DIR_1"
+mkdir -p "$TARGET_DIR_2"
+cp "$SOURCE_FILE" "$TARGET_FILE_1" && echo "Copied $SOURCE_FILE to $TARGET_FILE_1"
+cp "$SOURCE_FILE" "$TARGET_FILE_2" && echo "Copied $SOURCE_FILE to $TARGET_FILE_2"
