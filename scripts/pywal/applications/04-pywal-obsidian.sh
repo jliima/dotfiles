dc12dd1887f62e61d5da3c53c42f8e17ccc69a86
jliima <liimatainen.jere@gmail.com>
2025-09-21 11:31:46 +0300

    Modified Obsidian config, fixed obsidian pywal script.

--- scripts/pywal/applications/04-pywal-obsidian.sh
+++ scripts/pywal/applications/04-pywal-obsidian.sh
@@ -6,13 +6,17 @@ CACHE_DIR="$HOME/.cache/wal"
 
 
 SOURCE_FILE="$CACHE_DIR/colors-obsidian.css"
-TARGET_DIR_1="$HOME/Obsidian/default_vault/.obsidian/themes/PywalColors"
-TARGET_DIR_2="$HOME/Obsidian/work_vault/.obsidian/themes/PywalColors"
+TARGET_DIR_1="$HOME/Obsidian/default-vault/.obsidian/themes/PywalColors"
+TARGET_DIR_2="$HOME/Obsidian/work-vault/.obsidian/themes/PywalColors"
+TARGET_DIR_3="$HOME/Obsidian/school-vault/.obsidian/themes/PywalColors"
 TARGET_FILE_1="$TARGET_DIR_1/theme.css"
 TARGET_FILE_2="$TARGET_DIR_2/theme.css"
+TARGET_FILE_3="$TARGET_DIR_3/theme.css"
 
 # Create target directories if it doesn't exist
 mkdir -p "$TARGET_DIR_1"
 mkdir -p "$TARGET_DIR_2"
+mkdir -p "$TARGET_DIR_3"
 cp "$SOURCE_FILE" "$TARGET_FILE_1" && echo "Copied $SOURCE_FILE to $TARGET_FILE_1"
 cp "$SOURCE_FILE" "$TARGET_FILE_2" && echo "Copied $SOURCE_FILE to $TARGET_FILE_2"
+cp "$SOURCE_FILE" "$TARGET_FILE_3" && echo "Copied $SOURCE_FILE to $TARGET_FILE_3"
