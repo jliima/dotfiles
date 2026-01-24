da172e7d5275bdb3f857b45e18e987612950538a
jliima <liimatainen.jere@gmail.com>
2026-01-24 15:50:11 +0200

    Updated spicetify config.

--- scripts/pywal/applications/99-pywal-spicetify.sh
+++ scripts/pywal/applications/99-pywal-spicetify.sh
@@ -4,7 +4,6 @@ set -eu
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 CACHE_DIR="$HOME/.cache/wal"
 
-
 SOURCE_FILE="$CACHE_DIR/colors-spicetify.ini"
 TARGET_DIR="$HOME/.config/spicetify/Themes/pywal-theme"
 TARGET_FILE="$TARGET_DIR/color.ini"
@@ -12,5 +11,8 @@ TARGET_FILE="$TARGET_DIR/color.ini"
 # Create target directory if it doesn't exist
 mkdir -p "$TARGET_DIR"
 cp "$SOURCE_FILE" "$TARGET_FILE"
+
+# Remove all '#' characters from the target file
+sed -i 's/#//g' "$TARGET_FILE"
 
 spicetify apply
