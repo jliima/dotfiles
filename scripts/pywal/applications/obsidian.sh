36e2d3dd918583f2441ea6e7ea11471ff45b53da
jliima <liimatainen.jere@gmail.com>
2026-04-27 23:23:19 +0300

    Added custom dark pdf export thingy for obsidian and refined how mermaid charts look.

--- scripts/pywal/applications/obsidian.sh
+++ scripts/pywal/applications/obsidian.sh
@@ -2,10 +2,18 @@
 set -eu
 
 CACHE_DIR="$HOME/.cache/wal"
+OBSIDIAN_DIR="$HOME/Obsidian/.config/.obsidian-desktop"
 
-SOURCE_FILE="$CACHE_DIR/colors-obsidian.css"
-TARGET_DIR_1="$HOME/Obsidian/.config/.obsidian-desktop/themes/PywalColors"
-TARGET_FILE_1="$TARGET_DIR_1/theme.css"
+# Theme
+SOURCE_THEME="$CACHE_DIR/colors-obsidian.css"
+TARGET_THEME_DIR="$OBSIDIAN_DIR/themes/PywalColors"
+TARGET_THEME="$TARGET_THEME_DIR/theme.css"
 
-mkdir -p "$TARGET_DIR_1"
-cp "$SOURCE_FILE" "$TARGET_FILE_1" && echo "Copied $SOURCE_FILE to $TARGET_FILE_1"
+# PDF Export Darkmode snippet
+SOURCE_PDF="$CACHE_DIR/colors-obsidian-PDF-Export-Darkmode.css"
+TARGET_SNIPPETS_DIR="$OBSIDIAN_DIR/snippets"
+TARGET_PDF="$TARGET_SNIPPETS_DIR/PDF-Export-Darkmode.css"
+
+mkdir -p "$TARGET_THEME_DIR" "$TARGET_SNIPPETS_DIR"
+cp "$SOURCE_THEME" "$TARGET_THEME" && echo "Copied $SOURCE_THEME to $TARGET_THEME"
+cp "$SOURCE_PDF" "$TARGET_PDF" && echo "Copied $SOURCE_PDF to $TARGET_PDF"
