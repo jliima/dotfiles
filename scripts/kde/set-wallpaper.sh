91a56a5df9f8de900181a74e11b80e46d6f66cb7
jliima <liimatainen.jere@gmail.com>
2026-04-27 13:58:02 +0300

    Added new pepe, fixed wallpaper script.

--- scripts/kde/set-wallpaper.sh
+++ scripts/kde/set-wallpaper.sh
@@ -6,6 +6,6 @@ set -u
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 WALLPAPER_DIR="$HOME/Pictures/Wallpapers"
 
-"$SCRIPT_DIR/kde/set-wallpaper-for-activity.py" 'Default' "${HOME}/dotfiles/wallpapers/Along-the-Lunar-Terminator_cropped.jpg"
-"$SCRIPT_DIR/kde/set-wallpaper-for-activity.py" 'Work' "${WALLPAPER_DIR}/Misc/firm-logo-4k.png"
-"$SCRIPT_DIR/kde/set-wallpaper-for-activity.py" 'School' "${WALLPAPER_DIR}/Green/wallhaven-3lyzyv.jpg"
+"$SCRIPT_DIR/set-wallpaper-for-activity.py" 'Default' "${HOME}/dotfiles/wallpapers/wallhaven-3lyzyv_pare.jpg"
+"$SCRIPT_DIR/set-wallpaper-for-activity.py" 'Work' "${WALLPAPER_DIR}/Misc/firm-logo-4k.png"
+"$SCRIPT_DIR/set-wallpaper-for-activity.py" 'School' "${WALLPAPER_DIR}/Green/wallhaven-3lyzyv.jpg"
